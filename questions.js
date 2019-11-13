b6bf3023dca05c8a14aff4235a32bb08c3819e40
Amir <aamirvand97@gmail.com>
2019-11-13 13:29:54 -0800

    Had the wrong call for it... Had it set as question instead of questions... should be fixed

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-var question = [
+var questions = [
     ["What does `console.log()` do?", "Stores all the user information in your hard drive.", "Remembers the last thing you typed.", "Prints the variables that are defined within it.", "Keeps the information of all Hotdogs.", "C"],
     ["What does HTML stand for?", "Hypertext Markup Language", "Hotdog Taster : Major Leagues", "Hotmail", "High Temperature Materials Laboratory", "A"]
 ];
