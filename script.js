266f7aa15600ca510077189c1ee5394a355da088
Amir <aamirvand97@gmail.com>
2019-11-13 13:40:03 -0800

    Got everything working, setting this as the final product and pushing.

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
    var countdown;
    var countdownNumber;
 //Overall 
-   var test;
-   var testStatus;
+   var quiz;
+   var quizStatus;
    var question;
    //Tracks user choice
    var choice;
@@ -17,29 +17,29 @@
    
    function get(x) {
        return document.getElementById(x);
-    //Grabs the test questions from questions.js 
+    //Grabs the quiz questions from questions.js 
    }
    function renderQuestion(){
        containerEl.style.visibility = "visible";
        submitBtn.style.visibility = "hidden";
-       test = get("test");
+       quiz = get("quiz");
        if(pos >= questions.length){
            userScore = correct * countdown
-           test.innerHTML = "<h2>You got a score of "+userScore+"!<h2>";
-           get("test-status").innerHTML = "Test Completed, Congratulations!";
+           quiz.innerHTML = "<h2>You got a score of "+userScore+"!<h2>";
+           get("quiz-status").innerHTML = "Congratulations! You complete the Quiz!";
            countdownClear();
            return false;
        };
-       get("test-status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+       get("quiz-status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
        question = questions[pos][0];
        ansA = questions[pos][1];
        ansB = questions[pos][2];
        ansC = questions [pos][3];
-       test.innerHTML = "<h3>"+question+"<h3>";
-       test.innerHTML += "<input type='radio' name='choices' value='A'> "+ansA+"<br>";
-       test.innerHTML += "<input type='radio' name='choices' value='B'> "+ansB+"<br>";
-       test.innerHTML += "<input type='radio' name='choices' value='C'> "+ansC+"<br>";
-       test.innerHTML += "<button id='test-button' onclick='checkAnswer()' type='button' class='btn btn-secondary'>SUBMIT Answer</button>";
+       quiz.innerHTML = "<h3>"+question+"<h3>";
+       quiz.innerHTML += "<input type='radio' name='choices' value='A'> "+ansA+"<br>";
+       quiz.innerHTML += "<input type='radio' name='choices' value='B'> "+ansB+"<br>";
+       quiz.innerHTML += "<input type='radio' name='choices' value='C'> "+ansC+"<br>";
+       quiz.innerHTML += "<button id='quiz-button' onclick='checkAnswer()' type='button' class='btn btn-secondary'>SUBMIT Answer</button>";
 //Checks if the answer is correct / incorrect, gives user score.
    }
    function checkAnswer(){
@@ -55,8 +55,10 @@
        pos++;
        renderQuestion();
    }
+   
+//Base Timer Questions*15, should be 15 seconds per question, couldn't get formula to work.
    function countdownBegin() {
-       countdownNumber = 15;
+       countdownNumber = 30;
        countdownTrigger();
        renderQuestion();
    }
@@ -67,8 +69,8 @@
            countdown = setTimeout("countdownTrigger()", 1000)
        }
        else if (countdownNumber == 0) {
-           test.innerHTML = "<h2>You got a score of "+correct+"!<h2>";
-           get("test-status").innerHTML = "No time remaining.";
+           quiz.innerHTML = "<h2>You got a score of "+correct+"!<h2>";
+           get("quiz-status").innerHTML = "No time remaining.";
            pos = 0;
            correct = 0;
            countdownClear();
@@ -78,19 +80,18 @@
        clearTimeout(countdown);
        get("time-display").style.visibility = "hidden";
        localStorage.setItem("score", userScore)
-       var initials = prompt("Please enter your initials to save you score for next time");
-       localStorage.setItem("initials", initials);
+       var name = prompt("Please enter your name to save you score for next time");
+       localStorage.setItem("name", name);
    };
    get("user-score").innerHTML = "Previous score: "+localStorage.getItem("score");
-   get("user-initials").innerHTML = "Previous player: "+localStorage.getItem("initials");
+   get("user-name").innerHTML = "Previous player: "+localStorage.getItem("name");
    get("submit-button").addEventListener("click", countdownBegin);
 
 var submitBtn = document.getElementById("submit-button");
 var timeDisplay = document.getElementById("time-display");
 var containerEl = document.getElementById("container");
-
-//Base Timer Questions*15, should be 15 seconds per question, couldn't get formula to work. 
-var secondsLeft = 30;
+ 
+var secondsLeft = 75;
 
 function setTime() {
     renderQuestion();
