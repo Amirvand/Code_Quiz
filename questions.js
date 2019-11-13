0b088033c7b37de6e9dc7e0337880bfc3b7b18fc
Amir <aamirvand97@gmail.com>
2019-11-13 12:22:51 -0800

    added questions.js file

---
+++ questions.js
@@ -0,0 +1,4 @@
+var questions = [
+    ["What does `console.log()` do?", "Stores all the user information in your hard drive.", "Remembers the last thing you typed.", "Prints the variables that are defined within it.", "Keeps the information of all Hotdogs.", "C"],
+    ["What does HTML stand for?", "Hypertext Markup Language", "Hotdog Taster : Major Leagues", "Hotmail", "High Temperature Materials Laboratory", "A"]
+];
