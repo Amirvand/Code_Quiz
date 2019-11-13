b583350475ecbd66ff5f4bd63188e1705a33122e
Amir <aamirvand97@gmail.com>
2019-11-13 10:58:14 -0800

    added base variables to JS, added base layout for HTML with bootstrap.

---
+++ script.js
@@ -0,0 +1,8 @@
+//General variables
+var pos = 0;
+var correct = 0;
+var containerEl = document.getElementById("container");
+var submitBtn = document.getElementById("submit-button");
+//Timer
+var countdown;
+var countdownNumber;
